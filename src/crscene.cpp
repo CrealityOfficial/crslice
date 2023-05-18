86e7c3ee209aa77fbb7138b5101000b7dea08c8b
anoob <anoob@sina.cn>
2023-05-18 11:43:57 +0800

    remove some code
    Change-Id: Idc37574b5a7996cd5b691096a059960930e42729

--- src/crscene.cpp
+++ src/crscene.cpp
@@ -162,55 +162,6 @@ namespace crslice
 		in.close();
 	}
 
-	void CrScene::setMeshsTemps(int startTemp, int endTemp, int step)
-	{
-		CrGroup* currentGrop = getGroupsIndex(0);
-		if ((startTemp-endTemp)/step +1 == currentGrop->m_objects.size())
-		{
-			for (int n=0;n< currentGrop->m_objects.size();n++)
-			{
-				currentGrop->m_objects[n].m_settings->add("material_print_temperature",std::to_string(startTemp - step*n));
-			}
-		}
-	}
-
-	void CrScene::setMeshsHighFlow()
-	{
-		int startFlow = 0;
-		int endFlow = -9;
-		CrGroup* currentGrop = getGroupsIndex(0);
-		for (size_t i = 0; i < 10; i++)
-		{
-			currentGrop->m_objects[i].m_settings->add("wall_0_material_flow", std::to_string(100 -i));
-			currentGrop->m_objects[i].m_settings->add("wall_x_material_flow", std::to_string(100 - i));
-			currentGrop->m_objects[i].m_settings->add("skin_material_flow", std::to_string(100 - i));
-			currentGrop->m_objects[i].m_settings->add("roofing_material_flow", std::to_string(100 - i));
-			currentGrop->m_objects[i].m_settings->add("material_flow_layer_0", std::to_string(100 - i));
-			currentGrop->m_objects[i].m_settings->add("wall_x_material_flow_layer_0", std::to_string(100 - i));
-			currentGrop->m_objects[i].m_settings->add("wall_0_material_flow_layer_0", std::to_string(100 - i));
-			currentGrop->m_objects[i].m_settings->add("skin_material_flow_layer_0", std::to_string(100 - i));
-		}
-	};
-
-
-	void CrScene::setMeshsLowFlow()
-	{
-		int startFlow = 0;
-		int endFlow = -9;
-		CrGroup* currentGrop = getGroupsIndex(0);
-		for (size_t i = 0; i < 9; i++)
-		{
-			currentGrop->m_objects[i].m_settings->add("wall_0_material_flow", std::to_string(120 - i*5));
-			currentGrop->m_objects[i].m_settings->add("wall_x_material_flow", std::to_string(120 - i * 5));
-			currentGrop->m_objects[i].m_settings->add("skin_material_flow", std::to_string(120 - i * 5));
-			currentGrop->m_objects[i].m_settings->add("roofing_material_flow", std::to_string(120 - i * 5));
-			currentGrop->m_objects[i].m_settings->add("material_flow_layer_0", std::to_string(120 - i * 5));
-			currentGrop->m_objects[i].m_settings->add("wall_x_material_flow_layer_0", std::to_string(120 - i * 5));
-			currentGrop->m_objects[i].m_settings->add("wall_0_material_flow_layer_0", std::to_string(120 - i * 5));
-			currentGrop->m_objects[i].m_settings->add("skin_material_flow_layer_0", std::to_string(120 - i * 5));
-		}
-	};
-
 	CrGroup* CrScene::getGroupsIndex(int groupID)
 	{
 		if (groupID < m_groups.size())
