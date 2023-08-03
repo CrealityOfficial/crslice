6512f6f0686e8ce5861d4b342c6df07b982c321d
wangwenbin <wangwenbin@creality.com>
2023-08-03 13:39:58 +0800

    fix[crslice] 去掉多余的空格
    Change-Id: Ic212d12238ffb35111e03068be9e9f54ad77974f

--- impl/cura5.0/src/gcodeExport.cpp
+++ impl/cura5.0/src/gcodeExport.cpp
@@ -544,7 +544,7 @@ std::string GCodeExport::getFileHeader(const std::vector<bool>& extruder_is_used
                 prefix << "00.0000m";
             }
             prefix << new_line;
-            prefix << ";Layer height: " << application->current_slice->scene.current_mesh_group->settings.get<double>("layer_height") << new_line;
+            prefix << ";Layer height:" << application->current_slice->scene.current_mesh_group->settings.get<double>("layer_height") << new_line;
         }
         prefix << ";MINX:" << INT2MM(total_bounding_box.min.x) << new_line;
         prefix << ";MINY:" << INT2MM(total_bounding_box.min.y) << new_line;
