33ba6048797917c51530764853350a174b2ef774
wangwenbin <wangwenbin@creality.com>
2024-01-24 14:09:20 +0800

    fix[crslice] 修复支撑时顶面线条异常
    Change-Id: I9b2b7343e27621a66c8a24315e7be0e03d6eae2f

--- impl/cura5.0/magic/skin.cpp
+++ impl/cura5.0/magic/skin.cpp
@@ -327,12 +327,12 @@ void SkinInfillAreaComputation::generateRoofing(SliceLayerPart& part)
 			skin_part.skin_fill = skin_part.outline.intersection(no_air_above);
 		}
 
-		if (skin_part.roofing_fill.empty() && layer_nr > 0 && mesh.settings.get<bool>("support_enable"))
-		{
-			Polygons no_air_below = generateNoAirBelow(part, 1);
-			skin_part.below_fill = skin_part.outline.difference(no_air_below);
-			skin_part.skin_fill = skin_part.outline.intersection(no_air_below);
-		}
+		//if (skin_part.roofing_fill.empty() && layer_nr > 0 && mesh.settings.get<bool>("support_enable"))
+		//{
+		//	Polygons no_air_below = generateNoAirBelow(part, 1);
+		//	skin_part.below_fill = skin_part.outline.difference(no_air_below);
+		//	skin_part.skin_fill = skin_part.outline.intersection(no_air_below);
+		//}
 
         // Insets are NOT generated for any layer if the top/bottom pattern is concentric.
         // In this case, we still want to generate insets for the roofing layers based on the extra skin wall count,
