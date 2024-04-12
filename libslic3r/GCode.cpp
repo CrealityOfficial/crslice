b1a8a07a2607a26e987f14c8977c1eb992c9213e
weiyushuo <weiyushuo@creality.com>
2024-04-12 19:44:25 +0800

    fix:自定义gcode解析异常信息的多语言处理
    Change-Id: Ia02cd4008be8a385765cde2c037b7d9dd487cb4b

--- libslic3r/GCode.cpp
+++ libslic3r/GCode.cpp
@@ -2880,10 +2880,10 @@ void GCode::check_placeholder_parser_failed()
 {
     if (! m_placeholder_parser_integration.failed_templates.empty()) {
         // G-code export proceeded, but some of the PlaceholderParser substitutions failed.
-        std::string msg = Slic3r::format(_(L("Failed to generate gcode for invalid custom G-code.\n\n")));
+        std::string msg = Slic3r::format(_(L("Failed to generate gcode for invalid custom G-code.$$\n\n")));
         for (const auto &name_and_error : m_placeholder_parser_integration.failed_templates)
             msg += name_and_error.first + " " + name_and_error.second + "\n";
-        msg += Slic3r::format(_(L("Please check the custom G-code or use the default custom G-code.")));
+        msg += Slic3r::format(_(L("$$Please check the custom G-code or use the default custom G-code.")));
         throw Slic3r::PlaceholderParserError(msg);
     }
 }
