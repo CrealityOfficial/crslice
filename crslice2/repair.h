4353b5874d1eaff2e7257e1fadb34c8030b6b88e
zenggui <anoob@sina.cn>
2024-06-03 09:28:24 +0800

    feature:[]adaptive

---
+++ crslice2/repair.h
@@ -0,0 +1,8 @@
+#ifndef CRSLICE_SLICE_REPAIR_H_2
+#define CRSLICE_SLICE_REPAIR_H_2
+#include "crslice2/interface.h"
+
+namespace crslice2
+{
+}
+#endif // CRSLICE_SLICE_REPAIR_H_2
---
+++ src/repair.cpp
@@ -0,0 +1,8 @@
+#include "crslice2/repair.h"
+
+#include "libslic3r/Model.hpp"
+
+namespace crslice2
+{
+
+}
