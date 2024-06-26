a2eb5ea04a64230b9da339aa0092a5ac6afb3e35
weiyushuo <weiyushuo@creality.com>
2024-06-26 17:23:26 +0800

    fix:模型组内有零件悬空的警告信息
    Change-Id: If7a8a413df06ca278fce1b08793d7c2f7cd9ae70

--- wrapper/orcaslicewrapper.cpp
+++ wrapper/orcaslicewrapper.cpp
@@ -703,7 +703,7 @@ void orca_slice_impl_result(Slic3r::Print& print, Slic3r::Model& model, Slic3r::
 		save_parameter_2_json(temp_directory + "cx_parameter.json", model, config);
 
 	int alreadyShow = 0;
-	Slic3r::PrintBase::status_callback_type callback = [&tracer, &alreadyShow](const Slic3r::PrintBase::SlicingStatus& _status) {
+	Slic3r::PrintBase::status_callback_type callback = [&tracer, &alreadyShow, &print](const Slic3r::PrintBase::SlicingStatus& _status) {
 		if (tracer && alreadyShow <= _status.percent)
 		{
 			alreadyShow = _status.percent;
@@ -713,6 +713,24 @@ void orca_slice_impl_result(Slic3r::Print& print, Slic3r::Model& model, Slic3r::
 			if (tracer->interrupt())
 			{
 				throw Slic3r::SlicingError("User Cancelled", 0);
+			}
+		}
+
+		if (_status.percent < 0 && _status.warning_level == Slic3r::PrintStateBase::WarningLevel::NON_CRITICAL)
+		{
+			if (tracer)
+			{
+				if (_status.message_type == Slic3r::PrintStateBase::SlicingNeedSupportOn)
+				{
+					Slic3r::PrintObject* pObject = const_cast<Slic3r::PrintObject*>(print.get_object(_status.warning_object_id));
+					size_t sliceObjId = 0;
+					if (pObject && pObject->model_object())
+					{
+						sliceObjId = pObject->model_object()->id().id;
+					}
+
+					tracer->recordExtraMessage("SlicingNeedSupportOn", _status.text.c_str(), sliceObjId);
+				}
 			}
 		}
 	};
